b081be3d3073002936a81058bd9a290ab7435e72
Harry-Crocksick <harrycrocksick1inamillion@gmail.com>
2023-11-27 10:10:47 +0630

    Added tag

--- components/card.jsx
+++ components/card.jsx
@@ -4,6 +4,11 @@ import Stars from "./stars";
 export default function Card({ ...food }) {
   return (
     <section className="relative flex flex-col col-span-4 bg-[#231f20] text-white p-6 rounded-2xl mt-12">
+      {food.tag && (
+        <div className="absolute top-4 right-6 text-xs font-medium bg-[#c2ccce66] px-2 py-1 rounded">
+          {food.tag.toUpperCase()}
+        </div>
+      )}
       <div className="shrink-0 -mt-20 mb-10">
         <Image
           src={food.image}
